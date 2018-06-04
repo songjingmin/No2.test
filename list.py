67ae09707b4b039021c98d722261700603f0bb38
songjingmin <39300194+songjingmin@users.noreply.github.com>
2018-06-04 17:14:59 +0800

    Update list.py

--- list.py
+++ list.py
@@ -1,7 +1,7 @@
 # 使用列表
 shoplist = ['apple', 'mango', 'carrot', 'banana']
-print ("'I hanve', len(shoplist), 'items to purchase'")
+print("'I hanve', len(shoplist), 'items to purchase'")
 
-print ("'These items are:',"
-	for item in shoplist:
-        print ("item"))
+print("'These items are:',")
+for item in shoplist:
+	print("item"))
